aa0a0feb203594f76b373955e410bfe6f5af4f94
Piotr Ludwig <piotr.a.ludwig@gmail.com>
2021-02-13 19:33:34 +0100

    App interface

--- src/components/App.js
+++ src/components/App.js
@@ -1,27 +1,13 @@
 import React, { Component } from "react";
+import Web3 from "web3";
 import DaiToken from "../abis/DaiToken.json";
 import DappToken from "../abis/DappToken.json";
 import TokenFarm from "../abis/TokenFarm.json";
-import Web3 from "web3";
 import Navbar from "./Navbar";
 import Main from "./Main";
 import "./App.css";
 
 class App extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      account: "0x0",
-      daiToken: {},
-      dappToken: {},
-      tokenFarm: {},
-      daiTokenBalance: "0",
-      dappTokenBalance: "0",
-      stakingBalance: "0",
-      loading: true,
-    };
-  }
-
   async componentWillMount() {
     await this.loadWeb3();
     await this.loadBlockchainData();
@@ -29,6 +15,7 @@ class App extends Component {
 
   async loadBlockchainData() {
     const web3 = window.web3;
+
     const accounts = await web3.eth.getAccounts();
     this.setState({ account: accounts[0] });
 
@@ -45,7 +32,6 @@ class App extends Component {
       let daiTokenBalance = await daiToken.methods
         .balanceOf(this.state.account)
         .call();
-
       this.setState({ daiTokenBalance: daiTokenBalance.toString() });
     } else {
       window.alert("DaiToken contract not deployed to detected network.");
@@ -99,7 +85,66 @@ class App extends Component {
     }
   }
 
+  stakeTokens = (amount) => {
+    this.setState({ loading: true });
+    this.state.daiToken.methods
+      .approve(this.state.tokenFarm._address, amount)
+      .send({ from: this.state.account })
+      .on("transactionHash", (hash) => {
+        this.state.tokenFarm.methods
+          .stakeTokens(amount)
+          .send({ from: this.state.account })
+          .on("transactionHash", (hash) => {
+            this.setState({ loading: false });
+          });
+      });
+  };
+
+  unstakeTokens = (amount) => {
+    this.setState({ loading: true });
+    this.state.tokenFarm.methods
+      .unstakeTokens()
+      .send({ from: this.state.account })
+      .on("transactionHash", (hash) => {
+        this.setState({ loading: false });
+      });
+  };
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      account: "0x0",
+      daiToken: {},
+      dappToken: {},
+      tokenFarm: {},
+      daiTokenBalance: "0",
+      dappTokenBalance: "0",
+      stakingBalance: "0",
+      loading: true,
+    };
+  }
+
   render() {
+    console.log(this.state);
+    let content;
+    if (this.state.loading) {
+      content = (
+        <p id="loader" className="text-center">
+          Loading...
+        </p>
+      );
+    } else {
+      content = (
+        <Main
+          daiTokenBalance={this.state.daiTokenBalance}
+          dappTokenBalance={this.state.dappTokenBalance}
+          stakingBalance={this.state.stakingBalance}
+          stakeTokens={this.stakeTokens}
+          unstakeTokens={this.unstakeTokens}
+        />
+      );
+    }
+
     return (
       <div>
         <Navbar account={this.state.account} />
@@ -111,7 +156,13 @@ class App extends Component {
               style={{ maxWidth: "600px" }}
             >
               <div className="content mr-auto ml-auto">
-                <Main />
+                <a
+                  href="http://www.dappuniversity.com/bootcamp"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                ></a>
+
+                {content}
               </div>
             </main>
           </div>
